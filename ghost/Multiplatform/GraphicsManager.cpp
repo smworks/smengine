2b20e874c4af7bc2acec1f1469ef7d6715dd915f
SMartLT <martynas.su@gmail.com>
2014-11-10 14:41:38 +0200

    Commiting small changes.

--- ghost/Multiplatform/GraphicsManager.cpp
+++ ghost/Multiplatform/GraphicsManager.cpp
@@ -448,7 +448,7 @@ void GraphicsManager::renderScene(NodeType type) {
 #ifdef SMART_DEBUG
 		UINT64 start = getMicroseconds();
 #endif
-		glEnable(GL_DEPTH_TEST);
+		//glEnable(GL_DEPTH_TEST);
 		glEnable(GL_BLEND);
 		it = textArray_.begin();
 		while (it != textArray_.end()) {
@@ -458,7 +458,7 @@ void GraphicsManager::renderScene(NodeType type) {
 			it++;
 		}
 		glDisable(GL_BLEND);
-		glDisable(GL_DEPTH_TEST);
+		//glDisable(GL_DEPTH_TEST);
 #ifdef SMART_DEBUG
 	g_renderTextTime += (getMicroseconds() - start);
 #endif
