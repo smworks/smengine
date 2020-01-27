4221e58e193470e8c55a267cbfe5a3f9d75080f7
Martynas <martynas.su@gmail.com>
2020-01-27 11:28:36 +0200

    Disabled audio for android builds to work

--- ghost/Multiplatform/Android/AndroidSoundManager.h
+++ ghost/Multiplatform/Android/AndroidSoundManager.h
@@ -9,7 +9,7 @@
 
 #include "../SoundManager.h"
 
-#ifndef ENABLE_SOUND
+//#ifndef ENABLE_SOUND
 class AndroidSoundManager : public SoundManager {
 public:
 	AndroidSoundManager() {}
@@ -19,54 +19,54 @@ public:
 	void play(string name, bool repeat) {}
 	void update() {}
 };
-#else
-#include "../Ghost.h"
-#include "../../Resources/Sound.h"
-#include "../../../dependencies/includes/fmod/android/fmod.hpp"
-#include "../../../dependencies/includes/fmod/android/fmod_errors.h"
-
-#include <vector>
-#include <algorithm>
-
-class Martynas : public SoundManager {
-public:
-    Martynas();
-    ~Martynas();
-    	void reset() {}
-    	void play(Sound* sound) {}
-    	void play(string name, bool repeat) {}
-    	void update() {}
-};
-
-class AndroidSoundManager : public SoundManager {
-public:
-	AndroidSoundManager();
-	~AndroidSoundManager();
-
-	/**
-	 * @see SoundManager
-	 */
-	void reset();
-
-	/**
-	 * @see SoundManager
-	 */
-	void play(Sound* sound);
-
-	/**
-	 * @see SoundManager
-	 */
-	void play(string name, bool repeat);
-
-	/**
-	 * @see SoundManager
-	 */
-	void update();
-protected:
-	FMOD::System *soundSystem_;
-    FMOD::Channel* soundChannel_;
-	FMOD::Sound* sound_;
-};
-
-#endif
+//#else
+//#include "../Ghost.h"
+//#include "../../Resources/Sound.h"
+//#include "../../../dependencies/includes/fmod/android/fmod.hpp"
+//#include "../../../dependencies/includes/fmod/android/fmod_errors.h"
+//
+//#include <vector>
+//#include <algorithm>
+//
+//class Martynas : public SoundManager {
+//public:
+//    Martynas();
+//    ~Martynas();
+//    	void reset() {}
+//    	void play(Sound* sound) {}
+//    	void play(string name, bool repeat) {}
+//    	void update() {}
+//};
+//
+//class AndroidSoundManager : public SoundManager {
+//public:
+//	AndroidSoundManager();
+//	~AndroidSoundManager();
+//
+//	/**
+//	 * @see SoundManager
+//	 */
+//	void reset();
+//
+//	/**
+//	 * @see SoundManager
+//	 */
+//	void play(Sound* sound);
+//
+//	/**
+//	 * @see SoundManager
+//	 */
+//	void play(string name, bool repeat);
+//
+//	/**
+//	 * @see SoundManager
+//	 */
+//	void update();
+//protected:
+//	FMOD::System *soundSystem_;
+//    FMOD::Channel* soundChannel_;
+//	FMOD::Sound* sound_;
+//};
+//
+//#endif
 #endif
